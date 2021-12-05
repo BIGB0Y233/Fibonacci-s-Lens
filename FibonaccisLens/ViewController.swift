128fb13e6feaf9a39dce4e8585f5e17109d77304
Harry <harry.1016@icloud.com>
2021-12-05 23:15:20 +0800

    Initial Commit

---
+++ FibonaccisLens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FibonaccisLens
+//
+//  Created by Allan Shi on 2021/12/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
